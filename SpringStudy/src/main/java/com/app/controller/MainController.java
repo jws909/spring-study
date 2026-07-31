d53ad07800d08cace20aa9d227d4fb5edbe98364
jws909 <jws090909@gmail.com>
2026-07-31 15:22:25 +0900

    Request 요청 URL맵핑(컨트롤러), HTTP요청(GET/POST)

---
+++ SpringStudy/src/main/java/com/app/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller	//어노테이션
+public class MainController {
+	
+	@RequestMapping("/main")  //어떤 주소로 요청이 왔을때
+	public String main() {
+		
+		//return 하는 문자열 -> view 자원 이름
+		
+		return "main";
+	}
+}
