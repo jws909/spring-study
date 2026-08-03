4fd67d8fade8232ba74ed276eac4d6fee055eb0f
jws909 <jws090909@gmail.com>
2026-08-03 16:05:10 +0900

    quiz2 요청 파라미터 전달

---
+++ SpringStudy/src/main/java/com/app/controller/study/quiz/quiz01/Quiz02Controller.java
@@ -0,0 +1,38 @@
+package com.app.controller.study.quiz.quiz01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/quiz02")
+public class Quiz02Controller {
+
+	@GetMapping("/req1")
+	public String req1() {
+		
+		System.out.println("/quiz02/req1 GET 요청");
+		return "quiz/quiz02/req";
+	}
+	
+	@PostMapping("/req2")
+	public String req2() {
+		
+		System.out.println("/quiz02/req2 POST 요청");
+		return "quiz/quiz02/req";
+	}
+	
+	@RequestMapping("/req3")
+	public String req3(@RequestParam String data1, @RequestParam String data2) {
+		
+		System.out.println("/quiz02/req3 요청");
+		System.out.println("data1 : " + data1);
+		System.out.println("data2 : " + data2);
+		
+		return "quiz/quiz02/req";
+	}
+	
+	
+}
