3d53c6d3625fa7eb9e018ffb4d79f4d48c74c389
jws909 <jws090909@gmail.com>
2026-08-04 09:40:06 +0900

    lombok 라이브러리 의존성 추가

--- SpringStudy/src/main/java/com/app/dto/study/request/Product.java
+++ SpringStudy/src/main/java/com/app/dto/study/request/Product.java
@@ -1,32 +1,16 @@
 package com.app.dto.study.request;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Product {
 	
 	public String id;
 	public String name;
 	public int price;
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getPrice() {
-		return price;
-	}
-	public void setPrice(int price) {
-		this.price = price;
-	}
-	@Override
-	public String toString() {
-		return "Product [id=" + id + ", name=" + name + ", price=" + price + "]";
-	}
 	
 }
