5721d2764c21c395cf4aa5c8f2a9d82e89224797
jws909 <jws090909@gmail.com>
2026-08-13 14:46:10 +0900

    관리자페이지 임시 고객계정 추가하기, jdbc null 설정 추가

--- SpringStudy/src/main/java/com/app/service/user/UserService.java
+++ SpringStudy/src/main/java/com/app/service/user/UserService.java
@@ -1,5 +1,10 @@
 package com.app.service.user;
 
-public interface UserService {
+import com.app.dto.user.User;
 
+public interface UserService {
+	public int saveUser(User user);
+	
+	public int saveCustomerUser(User user);
+	public int saveAdminUser(User user);
 }
