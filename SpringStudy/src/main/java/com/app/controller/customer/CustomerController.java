9871dba2bd419129f87ce78e7e12f0fe8b5064c8
jws909 <jws090909@gmail.com>
2026-08-13 09:41:23 +0900

    DB연동 Mybatis 초기 설정

--- SpringStudy/src/main/java/com/app/controller/customer/CustomerController.java
+++ SpringStudy/src/main/java/com/app/controller/customer/CustomerController.java
@@ -3,6 +3,8 @@ package com.app.controller.customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import com.app.service.user.UserService;
+
 @Controller
 public class CustomerController {
 
