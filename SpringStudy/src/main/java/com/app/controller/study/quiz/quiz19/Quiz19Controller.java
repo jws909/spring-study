6593d1668cc98e777585d4bb18d19a038cda411d
jws909 <jws090909@gmail.com>
2026-08-19 12:24:45 +0900

    quiz 19 REST API 응답처리

---
+++ SpringStudy/src/main/java/com/app/controller/study/quiz/quiz19/Quiz19Controller.java
@@ -0,0 +1,39 @@
+package com.app.controller.study.quiz.quiz19;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.app.common.CommonCode;
+import com.app.dto.user.User;
+
+@Controller
+@RequestMapping("/quiz19")
+public class Quiz19Controller {
+	
+	@GetMapping("/quiz1")
+	public String quiz1() {
+		return "quiz/quiz19/quiz1";
+	}
+	
+	@ResponseBody
+	@GetMapping("/quiz2")
+	public String quiz2() {
+		return "return text quiz2";
+	}
+	
+	@ResponseBody
+	@GetMapping("/quiz3")
+	public User quiz3() {
+		
+		User user = new User();
+		
+		user.setId("quizid");
+		user.setPw("secret");
+		user.setName("quiz이름");
+		user.setUserType(CommonCode.USER_USERTYPE_CUSTOMER);
+		
+		return user;
+	}
+}
